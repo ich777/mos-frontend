19593bc12e7bae0af98d5bbc45b275526c88bf98
S3ppo <harald@moniholz.at>
2025-10-03 17:43:16 +0200

    Füge Unterstützung für Cron-Jobs hinzu: erweitere die Übersetzungen und optimiere die Logik zur Verwaltung von Cron-Jobs

--- src/views/mosSettings/cron.vue
+++ src/views/mosSettings/cron.vue
@@ -7,36 +7,36 @@
       <v-container fluid class="pa-0">
         <v-card variant="tonal">
           <v-card-text>
-            <div v-for="(job, index) in cron.cronJobs" :key="index" class="mb-4">
+            <div v-for="(cronJob, index) in cronJobs" :key="index" class="mb-4">
               <v-row>
                 <v-col cols="1" class="d-flex flex-column justify-center align-center">
                   <v-btn icon size="x-small" color="primary" class="pa-0"
                     style="width:24px; height:24px; min-width:24px; margin-bottom:6px;"
-                    @click="cron.cronJobs.splice(index + 1, 0, { name: '', schedule: '', command: '', enabled: false })"
+                    @click="cronJobs.splice(index + 1, 0, { name: '', schedule: '', command: '', enabled: false })"
                     title="Add cron job" aria-label="add cron job">
                     <v-icon size="18">mdi-plus</v-icon>
                   </v-btn>
                   <v-btn icon size="x-small" color="error" class="pa-0" style="width:24px; height:24px; min-width:24px;"
-                    @click="cron.cronJobs.splice(index, 1)" title="Remove cron job" aria-label="remove cron job">
+                    @click="cronJobs.splice(index, 1)" title="Remove cron job" aria-label="remove cron job">
                     <v-icon size="18">mdi-delete</v-icon>
                   </v-btn>
                 </v-col>
                 <v-col cols="11">
-                  <v-checkbox :label="$t('enabled')" v-model="job.enabled" density="compact"></v-checkbox>
-                  <v-text-field :label="$t('name')" v-model="job.name" density="compact"></v-text-field>
-                  <v-text-field :label="$t('schedule')" v-model="job.schedule" density="compact"></v-text-field>
-                  <v-text-field :label="$t('command')" v-model="job.command" density="compact"></v-text-field>
-                  <v-divider class="mt-4 mb-4"></v-divider>
+                  <v-checkbox :label="$t('enabled')" v-model="cronJob.enabled" density="compact"></v-checkbox>
+                  <v-text-field :label="$t('name')" v-model="cronJob.name" density="compact"></v-text-field>
+                  <v-text-field :label="$t('schedule')" v-model="cronJob.schedule" density="compact"></v-text-field>
+                  <v-text-field :label="$t('command')" v-model="cronJob.command" density="compact"></v-text-field>
+                  <v-divider></v-divider>
                 </v-col>
               </v-row>
             </div>
-            <div v-if="!cron.cronJobs || cron.cronJobs.length === 0" class="text-center">
+            <div v-if="!cronJobs || cronJobs.length === 0" class="text-center">
               {{ $t('no cron jobs available yet') }}
             </div>
             <div class="text-center">
               <v-btn icon size="x-small" color="primary" class="ma-1 pa-0"
                 style="width:24px; height:24px; min-width:24px;"
-                @click="cron.cronJobs.push({ name: '', schedule: '', command: '', enabled: false })"
+                @click="cronJobs.push({ id: '', name: '', schedule: '', command: '', enabled: false })"
                 title="Add cron job" aria-label="add cron job">
                 <v-icon size="18">mdi-plus</v-icon>
               </v-btn>
@@ -64,7 +64,7 @@ import { showSnackbarError, showSnackbarSuccess } from '@/composables/snackbar';
 import { useI18n } from 'vue-i18n';
 
 const emit = defineEmits(['refresh-drawer']);
-const cron = ref({ cronJobs: [] });
+const cronJobs = ref([]);
 const overlay = ref(false);
 const { t } = useI18n();
 
@@ -80,40 +80,77 @@ const getCron = async () => {
       }
     });
 
-    if (!res.ok) throw new Error(t('cron could not be loaded'));
-    cron.value = await res.json();
-    cron.value.cronJobs = cron.value.cronJobs || [];
+    if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error.error || t('cron jobs could not be loaded'));
+    }
+    cronJobs.value = await res.json();
 
   } catch (e) {
     showSnackbarError(e.message);
   }
 };
 
-const setCron = async (newCron) => {
+const createCron = async (cronJob) => {
   try {
-    overlay.value = true;
     const res = await fetch('/api/v1/cron', {
       method: 'POST',
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(newCron)
+      body: JSON.stringify(cronJob)
     });
-    overlay.value = false;
 
     if (!res.ok) throw new Error(t('cron jobs could not be changed'));
     showSnackbarSuccess(t('cron jobs changed successfully'));
-    emit('refresh-drawer');
 
   } catch (e) {
-    overlay.value = false;
     showSnackbarError(e.message);
   }
 };
 
-const setAllCrons = () => {
+const changeCron = async (cronJob) => {
+  try {
+    const res = await fetch('/api/v1/cron/' + cronJob.id , {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(cronJob)
+    });
 
+    if (!res.ok) throw new Error(t('cron jobs could not be changed'));
+    showSnackbarSuccess(t('cron jobs changed successfully'));
+
+  } catch (e) {
+    showSnackbarError(e.message);
+  }
+};
+
+const setAllCrons = async() => {
+  let error = false;
+  overlay.value = true;
+  cronJobs.value = cronJobs.value.filter(job => job.name.trim() !== '' || job.schedule.trim() !== '' || job.command.trim() !== '');
+  for (const job of cronJobs.value) {
+    try {
+      if (job.id && job.id !== '') {
+        await changeCron(job);
+      } else {
+        await createCron(job);
+      }
+    } catch (e) {
+      error = true;
+    }
+  }
+  overlay.value = false;
+  if (!error) {
+    showSnackbarSuccess(t('all cron jobs changed successfully'));
+    getCron();
+  } else {
+    showSnackbarError(t('some cron jobs could not be changed'));
+  }
 };
 
 </script>
