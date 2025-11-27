575436b4066816cf099fc6736df04c91f279ee67
S3ppo <harald@moniholz.at>
2025-11-27 14:26:40 +0100

    Entferne die Bedingung für die Anzeige des MOS Hub-Links im Navigationsmenü.

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@
           <v-list-item v-if="mosServices.docker?.enabled" to="/docker" prepend-icon="mdi-docker">
             <v-list-item-title>{{ $t('docker') }}</v-list-item-title>
           </v-list-item>
-          <v-list-item v-if="mosServices.docker?.enabled" to="/mosHub" prepend-icon="mdi-hub">
+          <v-list-item to="/mosHub" prepend-icon="mdi-hub">
             <v-list-item-title>{{ $t('mos hub') }}</v-list-item-title>
           </v-list-item>
           <v-list-item v-if="mosServices.lxc?.enabled" to="/lxc" prepend-icon="mdi-arrange-send-backward">
