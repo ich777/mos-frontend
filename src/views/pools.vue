c18f9960fed125355eca25ed4e6e40b1fb1857d2
S3ppo <harald@moniholz.at>
2026-08-11 12:58:16 +0200

    bugfix virtual pools policies

--- src/views/pools.vue
+++ src/views/pools.vue
@@ -2215,9 +2215,9 @@ const createVPool = async() => {
     options: {
       automount: createVpoolDialog.automount,
       comment: createVpoolDialog.comment,
-      policies: createVpoolDialog.config.policies,
     },
     config: {
+      policies: createVpoolDialog.config.policies,
       shared: createVpoolDialog.config.shared,
     },
   };
